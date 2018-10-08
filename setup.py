32d2ce61124b8eedf49786ea87c2d3ac8d24c8e1
makci97 <makci.97@mail.ru>
2018-10-08 21:57:40 +0300

    Add setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+"""Setup script."""
+
+from setuptools import setup
+
+setup(
+    name="hangman",
+    version="0.0.0",
+    author="Maxim Ivakhnenko",
+    author_email="makci.97@mail.ru",
+    url="https://github.com/makci97/hangman",
+    license="MIT",
+    packages=[
+    ],
+    install_requires=[
+        "numpy",
+    ],
+    setup_requires=[
+    ],
+    tests_require=[
+    ],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ]
+)
