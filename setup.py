a9be743246fc8fe3c2cb2ecaeb0ad0862eed0ffd
makci97 <makci.97@mail.ru>
2018-10-12 00:43:09 +0300

    Change setup

--- setup.py
+++ setup.py
@@ -12,18 +12,31 @@ setup(
     url="https://github.com/makci97/hangman",
     license="MIT",
     packages=[
+        "hangman",
     ],
     install_requires=[
         "numpy",
     ],
     setup_requires=[
+        "pytest-runner",
+        "pytest-pylint",
+        "pytest-pycodestyle",
+        "pytest-pep257",
+        "pytest-cov",
     ],
     tests_require=[
+        "pytest",
+        "pylint",
+        "pycodestyle",
+        "pep257",
     ],
     classifiers=[
         "Development Status :: 1 - Planning",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-    ]
+    ],
+    entry_points={
+        "console_scripts": ["hangman=hangman.hangman:main"]
+    }
 )
